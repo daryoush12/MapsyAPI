aa559ab29cc02dd7b6b9762a0d475a5edc84a468
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-06-20 21:29:02 +0300

    fix tests

--- src/test/java/repositories/PlaceRepositoryTests.java
+++ src/test/java/repositories/PlaceRepositoryTests.java
@@ -2,31 +2,30 @@ package repositories;
 
 
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mapsyapi.rest.models.Place;
 import com.mapsyapi.rest.repositories.PlaceRepository;
 
-import junit.framework.Assert;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration
+
+
+@SpringBootTest
 public class PlaceRepositoryTests {
 	
 	   @Autowired PlaceRepository repository;
 
-	    @SuppressWarnings("deprecation")
 		@Test
 	    public void readsFirstPageCorrectly() {
 
-	      Page<Place> places = repository.findAll(PageRequest.of(0, 10));
-	     
-	      Assert.assertTrue(places.isFirst());
+	      List<Place> places = repository.findAll();
+	      assertTrue(!places.isEmpty());
+	      
 	    }
 }
